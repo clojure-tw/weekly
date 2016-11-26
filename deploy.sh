1d262bc50d9fcfd14dbe8a9475fabd20f75104ac
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-11-26 20:23:58 +0800

    fix deploy.sh
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -38,4 +38,4 @@ set +e
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "git@github.com:clojure-tw/weekly.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
