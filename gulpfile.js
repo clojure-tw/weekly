fb4d857d9d052566fbc041a41c0f3f003e9cf78b
Yen-Chin, Lee <coldnew.tw@gmail.com>
2025-10-17 09:42:32 +0800

    fix gulp and nodejs deps
    Signed-off-by: Yen-Chin, Lee <coldnew.tw@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const gulp = require('gulp-help')(require('gulp'));
+const gulp = require('gulp');
 const bower = require('gulp-bower');
 const purify = require('gulp-purifycss');
 const concatCss = require('gulp-concat-css');
@@ -9,17 +9,17 @@ const gulpIgnore = require('gulp-ignore');
 const htmlreplace = require('gulp-html-replace');
 const gulpRemoveHtml = require('gulp-remove-html');
 const htmlmin = require('gulp-html-minifier');
-const gulpAmpValidator = require('gulp-amphtml-validator');
 const replace = require('gulp-replace');
-const uncss = require('gulp-uncss');
+const { exec } = require('child_process'); // ✅ 用於 AMP 驗證
+const fs = require('fs');
 
 const paths = {
   src: {
-    dir:  './resources/public/weekly',
+    dir: './resources/public/weekly',
     html: './resources/public/weekly/**/*.html'
   },
   dist: {
-    dir:  './build/dist',
+    dir: './build/dist',
     html: './build/dist/**/*.html'
   },
   tmp: {
@@ -28,24 +28,26 @@ const paths = {
   }
 };
 
-gulp.task('bower', function() {
+// --- Tasks ---
+
+gulp.task('bower', function () {
   return bower();
 });
 
-gulp.task('compile:css', ['bower'], function() {
-  return gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.min.css',
-                   './resources/public/weekly/css/screen.css'])
+gulp.task('compile:css', gulp.series('bower', function () {
+  return gulp.src([
+    './bower_components/bootstrap/dist/css/bootstrap.min.css',
+    './resources/public/weekly/css/screen.css'
+  ])
     .pipe(concatCss('app.css'))
     .pipe(purify([paths.src.html]))
-  // remove !importent which not supported by amphtml
+    // remove unsupported AMP syntax
     .pipe(replace(/!important/g, ''))
-  // remove @-ms-viewport which not supported by amphtml
     .pipe(replace(/@-ms-viewport\s*{\n(.*?)\n}/g, ''))
     .pipe(gulp.dest(paths.tmp.dir));
-});
+}));
 
-// inline-css inserts the cleaned + minified CSS into HTML
-gulp.task('build:inline-css', ['compile:css'], function() {
+gulp.task('build:inline-css', gulp.series('compile:css', function () {
   return gulp.src(paths.src.html)
     .pipe(htmlreplace({
       'cssInline': {
@@ -54,48 +56,51 @@ gulp.task('build:inline-css', ['compile:css'], function() {
       }
     }))
     .pipe(gulp.dest(paths.dist.dir));
-});
+}));
 
-gulp.task('build:remove-html', ['build:inline-css'], function () {
+gulp.task('build:remove-html', gulp.series('build:inline-css', function () {
   return gulp.src(paths.dist.html)
-    .pipe(gulpRemoveHtml({
-      'keyword': 'build:removeHtml'
+    .pipe(gulpRemoveHtml({ keyword: 'build:removeHtml' }))
+    .pipe(gulp.dest(paths.dist.dir));
+}));
+
+gulp.task('build:minify-html', gulp.series('build:remove-html', function () {
+  return gulp.src(paths.dist.html)
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCSS: true,
+      removeComments: true
     }))
     .pipe(gulp.dest(paths.dist.dir));
-});
+}));
 
-gulp.task('build:minify-html', ['build:remove-html'], function() {
+gulp.task('build:fix-html-link', gulp.series('build:minify-html', function () {
   return gulp.src(paths.dist.html)
-    .pipe(htmlmin({collapseWhitespace: true,
-                   minifyJS: true,
-                   minifyCSS: true,
-                   removeComments: true}))
-    .pipe(gulp.dest(paths.dist.dir));
-});
-
-gulp.task('build:fix-html-link', ['build:minify-html'], function () {
-  return gulp.src(paths.dist.html)
-  // make all link create new tab
     .pipe(replace(/<a\s+href="http/g, '<a target="_blank" href="http'))
     .pipe(gulp.dest(paths.dist.dir));
-});
+}));
 
+// ✅ New AMP Validation Task using official CLI
+gulp.task('validate:amphtml', gulp.series('build:minify-html', function (done) {
+  console.log('🔍 Running AMP validation...');
+  
+  exec(`npx amphtml-validator ${paths.dist.html}`, (err, stdout, stderr) => {
+    if (stdout) console.log(stdout);
+    if (stderr) console.error(stderr);
+    
+    if (err) {
+      console.error('❌ AMP validation failed.');
+      done(new Error('AMP validation failed.'));
+    } else {
+      console.log('✅ AMP validation passed.');
+      done();
+    }
+  });
+}));
 
-gulp.task('validate:amphtml', ['build:minify-html'], function() {
-return gulp.src(paths.dist.html)
-  // Some page no need to support amp, just ignore it
-    .pipe(gulpIgnore.exclude(['**/404.html']))
-  // Valide the input and attach the validation result to the "amp" property
-  // of the file object.
-    .pipe(gulpAmpValidator.validate())
-  // Print the validation results to the console.
-    .pipe(gulpAmpValidator.format())
-  // Exit the process with error code (1) if an AMP validation error
-  // occurred.
-    .pipe(gulpAmpValidator.failAfterError());
-});
-
-gulp.task('build', 'build all resources', [
+// --- Build sequence ---
+gulp.task('build', gulp.series(
   'bower',
   'compile:css',
   'build:inline-css',
@@ -103,8 +108,6 @@ gulp.task('build', 'build all resources', [
   'build:minify-html',
   'build:fix-html-link',
   'validate:amphtml'
-]);
+));
 
-gulp.task('default', 'build all resources', [
-  'build',
-]);
+gulp.task('default', gulp.series('build'));
